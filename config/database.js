ff41607d2b1eb24299c2129df1d1c30ecbbeaded
Alexander Chow-Yen <89674474+alexanderchowyen@users.noreply.github.com>
2021-10-29 23:11:25 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://devchow:devchow@cluster0.julzg.mongodb.net/baristaOrder?retryWrites=true&w=majority', 
+    'dbName': 'baristaOrder'
+};
+
